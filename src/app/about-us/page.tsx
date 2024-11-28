c30ef0ce435d1a00f73074ca6566e1cd21fe1570
muhammad_qurban11 <qurbanbhutto11@gmail.com>
2024-11-28 19:08:19 +0500

    Created about us Page and added some images.

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,11 +1,26 @@
-import { Img } from "@chakra-ui/react"
+import { Box, Heading, Image, Text } from "@chakra-ui/react"
 
 import React from 'react'
 export default function About() {
     return (
-        <div className="hover-container">
-            <Img src="/assets/contact us/contact_us.jpg" alt="Image" className="hover-image" />
-            <div className="hover-text">What can I help with?</div>
-        </div>
+        <Box>
+            <Box aria-label='cover-section' bgImage={`url("/assets/about us/baby-cover-image1.png")`}>
+                <Image src="/assets/about us/baby-cover-image1.png" alt="cover-image" w="full" />
+                <Box gap="4" position="absolute" top="50%" left="5%" px="4" display="flex" flexDirection="column" justifyContent="center" alignItems="flex-start" zIndex="1">
+                    <Heading color="app.primary">About Us</Heading>
+                    <Text justifyContent='left' textAlign='justify' w='40%'> Kangaroo Care AI system is designed with advanced features that make neonatal care easier, more efficient, and more effective.
+                        With intuitive controls and real-time monitoring.</Text>
+                </Box>
+            </Box>
+            <Box aria-label='our-mission-section' textAlign='center' mt='10'><Heading justifyContent='center' color='app.primary' mt="10">Featured In Shark Tank Pakistan 2024</Heading>
+                <Box px='8' py='6'>
+                    <Image src='/assets/about us/shark-tank-pakistan.jpg' alt='image_shark_tank' w='100%' h='100%' />
+                </Box>
+                <Text textAlign='justify' p='8'>Kangaroo Care AI, a cutting-edge startup focused on leveraging artificial intelligence to enhance infant care, has secured a major investment deal worth 3 crore through the Shark Tank Pakistan program. The investment will help the company scale its AI-driven solutions for monitoring infant health, development, and well-being. This deal is a significant step toward advancing technology in early childhood care, ensuring safer, smarter solutions for parents and healthcare professionals. With the support of the Sharks, Kangaroo Care AI aims to expand its impact and bring innovative products to the market. The partnership highlights the growing potential of AI in healthcare and infant care sectors</Text>
+                <Heading color='app.primary'>Our&nbsp;Mission</Heading>
+                <Text justifyContent='center' textAlign='justify' p='8'>Our mission is to empower and support innovative startups focused on harnessing the potential of artificial intelligence to improve early childhood development. We provide a nurturing environment for entrepreneurs to develop AI-driven solutions that enhance infant care, health monitoring, and learning. Through mentorship, resources, and funding, we aim to accelerate the creation of AI technologies that prioritize the well-being of infants. Our goal is to ensure these solutions are safe, ethical, and impactful, contributing to healthier, smarter futures for the next generation</Text>
+
+            </Box>
+        </Box>
     )
 }
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -30,16 +30,21 @@ export const Team = () => {
                     We bring together a diverse group of innovators, researchers, and engineers passionate about shaping the future of artificial intelligence. Our team works tirelessly to foster the growth of AI startups and entrepreneurs by providing cutting-edge resources, mentorship, and a collaborative environment.
                 </Text>
             </Box>
-            <Box className='hovering' display="flex" flexDirection="column" alignItems="center" justifyContent="center" >
-                <Flex gap='60' justifyContent="center" alignItems="center" >
-                    <Image src='/assets/team/team_img3.png' alt='ceo_img.png' w='25%' h='25%' />
-                    <Image src='/assets/team/team_img_05.png' alt='ceo_img.png' w='30%' h='30%' />
+            {/* <Box>
+                <Flex>
+                    <Image src='/assets/team/team_img3.png' alt='ceo_img.png' className='hover-image' w='40%' h="20%" />
+                    <Text className="hover-text" color='maroon'>Hovering Effect</Text>
+                    <Image src='/assets/team/team_img_05.png' alt='ceo_img.png' className="hover-image2" />
+                    <Text className="hover-text2">Text Hovering effect2</Text>
                 </Flex>
-                <Flex gap='60' justifyContent="center" alignItems="center" >
-                    <Image src='/assets/team/team_img3.png' alt='ceo_img.png' w='25%' h='25%' />
-                    <Image src='/assets/team/team_member.png' alt='ceo_img.png' w='30%' h='30%' />
+                <Flex>
+                    <Image src='/assets/team/team_img3.png' alt='ceo_img.png' className="hover-image3" />
+                    <Text className="hover-text3">Text Hovering effect3</Text>
+                    <Image src='/assets/team/team_member.png' alt='ceo_img.png' className="hover-image4" />
+                    <Text className="hover-text4">Text Hovering effect4</Text>
                 </Flex>
-            </Box>
+            </Box> */}
+
         </Box>
     )
 }
